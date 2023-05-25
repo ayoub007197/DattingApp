02707ad0d27e5b9bf51f2ba5a74bf3ab991ddccd
Ayoub <ayouhmadi@gmail.com>
2023-05-25 20:11:08 +0100

    Ajoutez des fichiers projet.

---
+++ DattingApp/Controllers/UsersController.cs
@@ -0,0 +1,40 @@
+﻿using DattingApp.Data;
+using DattingApp.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DattingApp.Controllers
+{
+    [ApiController]
+    [Route("{api/controller}")] // api/Users
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UsersController(DataContext context)
+        {
+            _context = context;
+            
+        }
+        [HttpGet]
+
+        public async  Task<ActionResult <IEnumerable<AppUser>>> GetUsers() {
+        
+            // we use actionResult as a return type so we can get the status of our Http request exple : 200 OK
+            var users= await _context.Users.ToListAsync();
+            return users;
+        
+        }
+
+        [HttpGet("{id}")]
+
+        public async Task< ActionResult<AppUser>> GetUser(int id)
+        {
+            
+            // we use actionResult as a return type so we can get the status of our Http request exple : 200 OK
+            
+            return await _context.Users.FindAsync(id);
+        }
+
+
+    }
+}
---
+++ DattingApp/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using DattingApp.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DattingApp.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options):base(options) { 
+        
+       
+        }
+
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
